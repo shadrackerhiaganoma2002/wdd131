7639a291d85f82f9f924eb6285372899dcdcc281
shadrackerhiaganoma2002 <erhiaganomas@gmail.com>
2025-01-17 17:38:52 +0100

    recreated my home page

--- getdates.js
+++
@@ -1,7 +0,0 @@
-const currentYearSpan = document.getElementById("currentyear");
-const currentYear = new Date().getFullYear();
-currentYearSpan.textContent = currentYear;
-
-const lastModified = document.lastModified;
-const lastModifiedElement = document.getElementById("lastModified");
-lastModifiedElement.textContent = `Last Modified: ${lastModified}`;
